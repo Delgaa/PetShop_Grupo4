4a7720c26c8f72d10c0be62293531fea58a23c57
Delgaa <112789365+Delgaa@users.noreply.github.com>
2024-01-17 18:41:33 -0300

    agregue eventos al carrito

--- assets/module/funciones.js
+++ assets/module/funciones.js
@@ -9,15 +9,15 @@ export function pintarTarjetas(arrArticulos, elemento, carrito_p) {
     const $articles =  document.querySelectorAll('article')
     $articles.forEach(article => {
         if (article.dataset.disponibles == 0) {
-            article.firstElementChild.textContent = 'Sin Stock!'
+            article.firstElementChild.textContent = '¡Sin Stock!'
             article.lastElementChild.lastElementChild.innerHTML =''
             article.lastElementChild.previousElementSibling.firstElementChild.firstElementChild.classList.replace('text-green-600', 'text-red-500')
         }else if (article.dataset.disponibles < 5) {
-            article.firstElementChild.textContent = 'Ultimas unidades!'
+            article.firstElementChild.textContent = '¡Ultimas unidades!'
         }
     })
 
-    const $buttons = document.querySelectorAll('button')
+    const $buttons = document.querySelectorAll('.button')
     $buttons.forEach(button => {
         for (const articulo of carrito_p) {
             button.dataset.id == articulo.id ? button.firstElementChild.classList.replace('bg-slate-400', 'bg-green-400'): ''
@@ -30,10 +30,10 @@ export function pintarTarjetas(arrArticulos, elemento, carrito_p) {
 export function tarjetaCarrito(carrito_p) {
     return `
     <article class='flex items-center border  justify-between hover:bg-gray-200'>
-    <img class = ' w-[50px] ' src ='${carrito_p.imagen}' alt = 'imagen de ${carrito_p.producto}'>
-    <h3 class = 'w-[100px] text-xs font-thin'>${carrito_p.producto}</h3>
-    <p class = 'text-xs font-thin'>$${carrito_p.precio}</p>
-    <button class = 'text-blue-400 text-xs font-thin'>Eliminiar</button>
+        <img class = ' w-[50px] ' src ='${carrito_p.imagen}' alt = 'imagen de ${carrito_p.producto}'>
+        <h3 class = 'w-[100px] text-xs font-thin'>${carrito_p.producto}</h3>
+        <p class = 'text-xs font-thin'>$${carrito_p.precio}</p>
+        <button data-id ='${carrito_p.id}' class = 'text-blue-600 text-xs font-thin hover:text-red-600'>Eliminar</button>
     </article>
     `
 }
@@ -43,7 +43,7 @@ export function imprimirCarrito(arrCarrito, elemento) {
         tarjeta += tarjetaCarrito(articulo)
     )
     elemento.innerHTML = tarjeta
-    elemento.innerHTML += '<button class="mt-auto text-blue-400 text-xs font-thin py-2 hover:bg-gray-200">Vaciar Carrito</button>'
+    elemento.innerHTML += '<button class="mt-auto text-blue-600 text-xs font-thin py-2 hover:bg-gray-200">Vaciar Carrito</button>'
 }
 
 export function crearTarjeta(artJuguetes) {
@@ -59,7 +59,7 @@ export function crearTarjeta(artJuguetes) {
         <div class = 'flex justify-around pt-4'>
             <a class ='p-1 bg-blue-500 self-start rounded-lg text-white' href = '?id=${artJuguetes._id}'>Mas detalles</a>
             <div>
-            <button data-id ='${artJuguetes._id}' data-producto = '${artJuguetes.producto}' data-precio= '${artJuguetes.precio}' data-imagen = '${artJuguetes.imagen}'>
+            <button class = 'button' data-id ='${artJuguetes._id}' data-producto = '${artJuguetes.producto}' data-precio= '${artJuguetes.precio}' data-imagen = '${artJuguetes.imagen}'>
                 <i class="p-2 bg-slate-400 rounded-lg fa-solid fa-cart-shopping"></i>
             </button>
             </div>
--- assets/script/juguetes.js
+++ assets/script/juguetes.js
@@ -21,27 +21,47 @@ fetch('https://moviestack.onrender.com/api/petshop')
         $busqueda.addEventListener('input', ()=> {
             const valorBusqueda = $busqueda.value
             const producto = articulosJuguetes.filter(articulo => articulo.producto.toLowerCase().includes(valorBusqueda.toLowerCase()))
+            carrito = JSON.parse(localStorage.getItem('carrito'))
             pintarTarjetas(producto, $jugetes, carrito)
             producto.length == 0 ? $jugetes.innerHTML = '<p class= "text-xl fond-medium">No se encontro articulo ingresado. Intente de nuevo</p>': ''
         })
 
+        $carrito.addEventListener('click', (e) => {
+            if (e.target.textContent == 'Eliminar') {
+                const ubicacion = carrito.findIndex(articulo => articulo.id == e.target.dataset.id)
+                carrito.splice(ubicacion, 1) 
+                const $buttons = document.querySelectorAll('.button')
+                $buttons.forEach(buton =>{
+                    if (buton.dataset.id == e.target.dataset.id) {
+                        buton.firstElementChild.classList.replace('bg-green-400', 'bg-slate-400')
+                    }
+                })
+                localStorage.setItem('carrito', JSON.stringify(carrito))
+                imprimirCarrito(carrito, $carrito)
+            }
+            if (e.target.textContent == 'Vaciar Carrito') {
+                carrito = []
+                localStorage.setItem('carrito', JSON.stringify(carrito))
+                imprimirCarrito(carrito, $carrito)
+                pintarTarjetas(articulosJuguetes, $jugetes, carrito)
+            }
+        })
+
         $mostrarCarrito.addEventListener('click',(e)=>{
-            $mostrarCarrito.firstElementChild.classList.toggle('hidden')
-            console.log(e.target);
+            if (e.target.id == 'carrito') {
+                $mostrarCarrito.firstElementChild.classList.toggle('hidden')
+            }
         })
 
         $jugetes.addEventListener('click', (e) => {
             if (e.target.classList.contains('bg-slate-400')) {
-
                 e.target.classList.replace('bg-slate-400', 'bg-green-400')
-
                 const idCarrito = {
                     id: e.target.parentElement.dataset.id,
                     producto: e.target.parentElement.dataset.producto,
                     imagen: e.target.parentElement.dataset.imagen,
                     precio: e.target.parentElement.dataset.precio
                 }
-
                 carrito.push(idCarrito)
                 imprimirCarrito(carrito, $carrito)
                 localStorage.setItem('carrito', JSON.stringify(carrito))
